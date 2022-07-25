3a3ec000fe59676692a13a2a7346607892ceeced
rephaelgo23-meet <109904475+rephaelgo23-meet@users.noreply.github.com>
2022-07-25 10:29:54 +0300

    nbhghty

---
+++ main.js
@@ -0,0 +1,9 @@
+function changeBackgroundColor(color){
+	document.body.style.background = color;
+}
+
+changeBackgroundColor("lightgreen");
+
+function linkcolor(color){
+	document.getElementsByTagName("a")[0].style.color = color;
+}
